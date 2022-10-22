cabcde6220f4c38d1b2340fc550a6882d1c22843
OlgaResh <114303017+OlgaResh@users.noreply.github.com>
2022-10-22 16:22:52 +0200

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int x,y,z;
+    cout << "imput x:";
+    cin >> x;
+    cout << "imput y:";
+    cin >> y;
+    z = x + y;
+   cout << "x+y=" << z << endl;
+}
